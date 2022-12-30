f62ee50dafa72f3978c08d386c7aac0381e3495c
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-12-30 13:01:24 +0600

    added Dashboard placeholder

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,1 +1,17 @@
-export {};
+import React from 'react';
+import { ScrollRestoration } from 'react-router-dom';
+import Navbar from '../pages/shared/Navbar';
+
+const Dashboard = () => {
+  return (
+    <>
+      <ScrollRestoration />
+      <Navbar />
+      <h1 className="container mx-auto text-3xl px-4 min-h-[85vh] grid place-content-center">
+        Coming soon...
+      </h1>
+    </>
+  );
+};
+
+export default Dashboard;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
+import Dashboard from '../layouts/Dashboard';
 import Main from '../layouts/Main';
 import NotFound from '../pages/NotFound';
 
 const router = createBrowserRouter([
   { path: '/', element: <Main />, children: [], errorElement: <NotFound /> },
+  {
+    path: '/dashboard',
+    element: <Dashboard />,
+    children: [],
+    errorElement: <NotFound />,
+  },
 ]);
 
 export default router;
