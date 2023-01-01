dbad19b73958cfe6e1fc374a754631650466b551
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2023-01-01 12:05:54 +0600

    responsive DigitalClock

--- src/components/ui/DigitalClock.tsx
+++ src/components/ui/DigitalClock.tsx
@@ -23,16 +23,52 @@ const DigitalClock = () => {
   }, [clock]);
 
   return (
-    <figure className="">
-      <div className="container px-4 mx-auto">Digital Clock</div>
-      <p className="">{moment(count).format('hh:mm:ss A')}</p>
-      <div className="">
-        <p className="">{moment(count).format('dddd')}</p>
-        <p className="">{moment.weekdays()}</p>
+    <figure className="font-['digital7'] py-8 drop-shadow ">
+      <div className="container px-4 mx-auto">
+        <div className="grid gap-4 xl:gap-12 p-4 xl:p-8 border-4 border-emerald-500 shadow-xl">
+          <p className="text-gray-200 text-xl flex gap-4 flex-wrap justify-center sm:justify-between">
+            {moment.weekdays().map((day, i) => (
+              <span
+                key={`day-${i + 1}`}
+                className={`${
+                  moment(count).format('dddd') === day
+                    ? 'text-emerald-500 font-bold'
+                    : ''
+                }`}
+              >
+                {day}
+              </span>
+            ))}
+          </p>
+          <div className="flex flex-wrap gap-8 xl:gap-12 justify-around items-center">
+            <div className="flex flex-wrap gap-4 items-center">
+              <p className="relative isolate z-10 text-4xl xl:text-6xl">
+                <span className="absolute top-0 left-0 text-gray-200 -z-10">
+                  88
+                </span>
+                <span className="text-emerald-500">
+                  {moment(count).format('DD')}
+                </span>
+              </p>
+              <div className="flex flex-col gap-1">
+                <p className="">{moment().format('MMM')}</p>
+                <p className="">{moment().year()}</p>
+              </div>
+            </div>
+            <div className="relative isolate text-6xl xl:text-8xl text-center">
+              <p className="absolute top-0 left-0 text-gray-200 -z-10">
+                88:88:88 88
+              </p>
+              <time
+                className="text-emerald-500"
+                dateTime={moment(count).toString()}
+              >
+                {moment(count).format('hh:mm:ss A')}
+              </time>
+            </div>
+          </div>
+        </div>
       </div>
-      <p className="">{moment().date()}</p>
-      <p className="">{moment().format('MMM')}</p>
-      <p className="">{moment().year()}</p>
     </figure>
   );
 };
